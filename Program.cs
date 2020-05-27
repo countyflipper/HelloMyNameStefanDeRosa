bd23075c08ad520bdbff84630e1dae37675ec292
sderosa <C0astal89>
2020-05-27 19:09:05 -0400

    Feature: Added new code with Car (Make/Model) as well as ReadMe.md

--- Program.cs
+++ Program.cs
@@ -9,7 +9,10 @@ namespace HelloMyNameStefanDeRosa
             Console.WriteLine("Hello World!");
             string szfname = "Stefan";
             string szlname = "DeRosa";
+            string szMake = "Hyundai";
+            string szModel = "Santa Fe";
             Console.WriteLine("Hello my name is {0} {1}", szfname, szlname);
+            Console.WriteLine("I have the following car make {0} and model {1}", szMake, szModel);
 
         }
     }
---
+++ README.md
@@ -0,0 +1,5 @@
+﻿# Homework: Create a Command Line Application
+For this homework assignment, you should create a command line application using C# and Visual Studio.  
+
+# Screenshot
+![postman request output](Images/Stefan DeRosa Screenshot.png)
